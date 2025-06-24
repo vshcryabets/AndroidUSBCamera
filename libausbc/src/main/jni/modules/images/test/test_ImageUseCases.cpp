6796c0559d92103dc09af4e2b5086215a3486bb4
V.Shkriabets <vshcryabets@gmail.com>
2025-06-24 21:41:25 +0300

    Add YUV420p converter

--- libausbc/src/main/jni/modules/images/test/test_ImageUseCases.cpp
+++ libausbc/src/main/jni/modules/images/test/test_ImageUseCases.cpp
@@ -6,9 +6,9 @@ TEST_CASE("ConvertYUV420ptoRGBAUseCase", "[ImageUseCases]") {
     ConvertBitmapUseCase::Buffer src;
     uint8_t sampleYuvData[] = { 
         // Y
+        0x7F, 0x7F, 76, 76, // Grey, Red (255)
         0x7F, 0x7F, 76, 76,
-        0x7F, 0x7F, 76, 76,
-        149, 149, 29, 29,
+        149, 149, 29, 29, // Green, Blue
         149, 149, 29, 29,
         // U
         0x80, 84,
@@ -24,6 +24,32 @@ TEST_CASE("ConvertYUV420ptoRGBAUseCase", "[ImageUseCases]") {
     src.height = 4;
 
     auto result = useCase.convertToNew(src);
-    
-    REQUIRE(1 == 1);
+    REQUIRE(result.buffer != nullptr);
+    REQUIRE(result.capacity == 16*4);
+    REQUIRE(result.size == 16*4);
+    REQUIRE(result.width == 4);
+    REQUIRE(result.height == 4);
+    // grey pixels
+    REQUIRE((int)result.buffer[3] == 255);
+    REQUIRE(abs((int)result.buffer[0] - 128) < 2); // Allow some tolerance for rounding
+    REQUIRE(abs((int)result.buffer[1] - 128) < 2);
+    REQUIRE(abs((int)result.buffer[2] - 128) < 2);
+
+    // red pixels
+    REQUIRE(abs((int)result.buffer[24] - 255) < 2);
+    REQUIRE(abs((int)result.buffer[25] - 0) < 2);
+    REQUIRE(abs((int)result.buffer[26] - 0) < 2);
+    REQUIRE((int)result.buffer[27] == 255);
+
+    // green pixels
+    REQUIRE(abs((int)result.buffer[32] - 0) < 2);
+    REQUIRE(abs((int)result.buffer[33] - 255) < 2);
+    REQUIRE(abs((int)result.buffer[34] - 0) < 2);
+    REQUIRE((int)result.buffer[35] == 255);
+
+    // blue pixels
+    REQUIRE(abs((int)result.buffer[56] - 0) < 2);
+    REQUIRE(abs((int)result.buffer[57] - 0) < 2);
+    REQUIRE(abs((int)result.buffer[58] - 255) < 2);
+    REQUIRE((int)result.buffer[59] == 255);
 }
