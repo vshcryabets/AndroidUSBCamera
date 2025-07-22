520684be5622fc6c43b9a04733d86732a73641d7
V.Shkriabets <vshcryabets@gmail.com>
2025-07-22 23:02:12 +0300

    Fix build for JNI on desktop

--- linux/libs/jnilib/src/main.cpp
+++ linux/libs/jnilib/src/main.cpp
@@ -1,9 +1,14 @@
 #include <jni.h>
+#include "FontWrapper.h"
+#include "JniSources.h"
 
 jint JNI_OnLoad(JavaVM *vm, void *reserved) {
     JNIEnv *env;
     if (vm->GetEnv(reinterpret_cast<void **>(&env), JNI_VERSION_1_6) != JNI_OK) {
         return JNI_ERR;
     }
+    FontWrapper_register(env);
+    JniSources_register(env);
+
     return JNI_VERSION_1_6;
 }
