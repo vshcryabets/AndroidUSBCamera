5c9f2be56d95bfab82c0d05828f15dd1f7e9f806
Volodymyr Shkriabets <vshcryabets@gmail.com>
2025-08-10 18:55:43 +0300

    Fix tests

--- workflow_build.py
+++ workflow_build.py
@@ -6,3 +6,20 @@ result = subprocess.run(
     cwd="linux/",
     check=True
 )
+
+# Gradle
+if "--prepare" in sys.argv:
+    subprocess.run(
+        ["chmod", "+x", "gradlew"],
+        check=True
+    )
+if "--build" in sys.argv:
+    subprocess.run(
+        ["./gradlew", "assembleDebug"],
+        check=True
+    )
+if "--test" in sys.argv:
+    subprocess.run(
+        ["./gradlew", "testDebugUnitTest"],
+        check=True
+    )
