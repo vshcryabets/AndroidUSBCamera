e39061b9d584f66e734cb89a00cd37ca43a37985
V.Shkriabets <vshcryabets@gmail.com>
2025-09-03 23:38:03 +0300

    Update decoder tests

--- libausbc/src/main/jni/modules/decoders/include/DecoderH264LibAVCodec.h
+++ libausbc/src/main/jni/modules/decoders/include/DecoderH264LibAVCodec.h
@@ -6,16 +6,19 @@ struct X264DecoderConfig : public DecoderBaseConfiguration
 {
 };
 
-class DecoderH264LibAVCodec : public Decoder, public DecoderWithConfiguration<X264DecoderConfig>
+class DecoderH264LibAVCodec : public Decoder,
+                              public DecoderWithConfiguration<X264DecoderConfig>
 {
 private:
-
 public:
     DecoderH264LibAVCodec();
     virtual ~DecoderH264LibAVCodec();
     virtual void open(const X264DecoderConfig &config) override;
-    void start() override;
-    void stop() override;
+    void startProducing(const Source::ProducingConfiguration &config) override;
+    void consume(const auvc::Frame &frame) override;
+    void stopConsuming() override;
+    void stopProducing() override;
     void close() override;
-    DecoderMultiBuffer decodeFrame(const DecoderBuffer &inputBuffer) override;
+    std::map<uint16_t, std::vector<Resolution>> getSupportedResolutions() const override;
+    std::vector<auvc::FrameFormat> getSupportedFrameFormats() const override;
 };
