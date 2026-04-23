ce2aa7ab73b10277bf3b84c2e1b3a3ac978d14a1
V.Shkriabets <vshcryabets@gmail.com>
2026-04-23 23:23:54 +0300

    Upgrade JUnit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -79,4 +79,5 @@ dependencies {
 
     testImplementation(libs.junit.jupiter.api)
     testRuntimeOnly(libs.junit.jupiter.engine)
+    testRuntimeOnly(libs.junit.platform.launcher)
 }
--- libausbc/build.gradle.kts
+++ libausbc/build.gradle.kts
@@ -56,6 +56,8 @@ dependencies {
     implementation(libs.androidx.constraintlayout)
     implementation(libs.material)
     implementation(libs.timber)
+
     testImplementation(libs.junit.jupiter.api)
     testRuntimeOnly(libs.junit.jupiter.engine)
+    testRuntimeOnly(libs.junit.platform.launcher)
 }
